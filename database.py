4e5c62b8e00b7d785fc026bf18e5f1d542f033ea
Kannell <johan.kannel@gmail.com>
2016-11-11 11:47:40 +0100

    nothing

--- database.py
+++ database.py
@@ -4,10 +4,12 @@ def runQuery(cursor,query):
     cursor.execute(query)
     return cursor.fetchone()
 
+
 #USERS:
 def createUser(cursor,name,ssn,adress,email,zipCode,phone,password):
     query = "INSERT INTO users (name,ssn,adress,email,zip_code,phone,password) VALUES (" + name + "," + ssn + "," + adress + "," + email + "," + zipCode + "," + phone + "," + password + ");"
     runQuery(cursor,query)
+
 
 def validateUser (cursor,userid,password):
     query = "SELECT password FROM users WHERE uid = " + userid
@@ -25,6 +27,7 @@ def insertOrder (cursor,customer,adress,cart,price):
 def insertCart (cursor,customer,item,price):
     query = "INSERT INTO carts (customer,items,price) VALUES ({}, {}, {})".format(customer,[item],price)
     runQuery(cursor,query)
+
 
 def modifyCart (cursor,cart,bool,product):
     query = "SELECT items FROM carts WHERE uid = {}".format(cart)
