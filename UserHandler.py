da860c1e600ba9668c76a4699ca2965bcdaf0a55
MrOwlie <fredrik.uvgard@gmail.com>
2016-12-09 15:08:51 +0100

    done

--- UserHandler.py
+++ UserHandler.py
@@ -7,18 +7,29 @@ class UserHandler:
         self.users = dict()
 
     def newUser(self, name, email, password, zipCode, city, address, phone, ssn):
-        ID = db.runQuery("SELECT MAX(uid) FROM users").fetchone()
-        seshID = uuid.uuid4
+        ID = self.db.runQuery("SELECT MAX(uid) FROM users").fetchone()
+        seshID = str(uuid.uuid4().hex)
         user = User.register(self.db, seshID, email, password, name, zipCode, city, address, phone, ssn)
         user.handler = self
         self.users[seshID] = user
         return seshID
 
     def returningUser(self, email, password):
-        seshID = uuid.uuid4
+        seshID = str(uuid.uuid4().hex)
+        print("returningUser() -> seshID: " + seshID)
         validated = self.db.validatePassword(email,password)
         if not validated:
             return "ERROR: Invalid email or password"
         user = User.login(self.db, seshID, email)
         self.users[seshID] = user
+        print("returningUser() -> users: " + str(self.users))
         return seshID
+
+    def isSessionActive(self, seshID):
+        try:
+            if(self.users[seshID] != None):
+                return True
+            else:
+                return False
+        except KeyError:
+            return False
