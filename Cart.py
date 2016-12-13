5d821a243301e66a5f42eabe73dd86721bdc7c6c
Kannell <johan.kannel@gmail.com>
2016-12-13 19:45:50 +0100

    sadas

--- Cart.py
+++ Cart.py
@@ -6,14 +6,14 @@ class Cart:
         self.db = db;
 
 
-    def newCart(self, userID):
+    def new(self, userID):
         self.products = []
         self.userID = userID
         self.db.runQuery("INSERT INTO carts (customer) VALUES ('{}')".format(userID))
         self.cartID = self.db.runQuery("SELECT uid FROM carts WHERE customer = '{}'".format(userID)).fetchone()
         return self.cartID
 
-    def getCart(self, userID):
+    def get(self, userID):
         self.userID = userID
         self.cartID = self.db.runQuery("SELECT uid FROM carts WHERE customer='{}'".format(userID)).fetchone()
         self.products = self.db.runQuery("SELECT details FROM products WHERE cart = '{}'".format(self.cartID)).fetchall()
@@ -41,9 +41,10 @@ class Cart:
             price = price + self.db.runQuery("SELECT price FROM product_details WHERE uid = '{}'".format(product)).fetchone()
         return price
 
-    def add(self, detailsID):
-        self.products.append(detailsID)
-        self.db.runQuery("INSERT INTO products (cart, details) VALUES ('{}','{}')".format(self.cartID, detailsID))
+    def add(self, detailsID, amount):
+        for _i in range(1,amount):
+            self.products.append(detailsID)
+            self.db.runQuery("INSERT INTO products (cart, details) VALUES ('{}','{}')".format(self.cartID, detailsID))
         return True
 
     def remove(self, detailsID):
